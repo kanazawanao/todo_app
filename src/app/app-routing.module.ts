c263f79c833eb80579e748ffbee04d056ad1adbb
Nao Kanazawa <n.kanazawa@w2solution.co.jp>
2019-02-21 19:00:10 +0900

    create dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoListComponent }      from './todo-list/todo-list.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'todoList', component: TodoListComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'todoList', component: TodoListComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.getTasks();
+  }
+ 
+  getTasks(): void {
+    this.taskService.getTasks()
+      .subscribe(tasks => this.tasks = tasks.slice(1, 5));
   }
 
 }
