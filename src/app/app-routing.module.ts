0925f2b3a16a76931d5b7faecb60ab1a651b2d59
Nao Kanazawa <n.kanazawa@w2solution.co.jp>
2019-02-25 16:00:07 +0900

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskDetailComponent }  from './task-detail/task-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'todoList', component: TodoListComponent },
+  { path: 'detail/:id', component: TaskDetailComponent },
   { path: 'dashboard', component: DashboardComponent }
 ];
 
